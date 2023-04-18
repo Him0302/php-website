00ed8ea8c4b0fc03b2be6b6e0360c473418c6b9a
Him0302 <81606642+Him0302@users.noreply.github.com>
2023-04-18 20:03:46 +0530

    Update index.php

--- php-mysql-website-master/web/index.php
+++ php-mysql-website-master/web/index.php
@@ -17,7 +17,18 @@
     <![endif]-->
 </head>
 <body>
+    
+    <body>
 
+    <h1>My first PHP page</h1>
+
+    <?php
+    echo "Hello World!";
+    ?>
+
+    </body>
+    
+    
     <div id="wrapper">
 
         <!-- Navigation -->
